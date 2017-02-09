9a20555ca1909d22cd829a9fd92b9d4e083d4374
Thom Brink <thombrink@hotmail.com>
2017-02-09 16:24:43 +0100

    added an array list implementation

---
+++ ArrayList/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new ArrayList(10);
+            list.Add(1);
+            list.Add(5);
+            list.Add(2);
+            Console.WriteLine(list.Get(0) == 1);
+            list.Set(0, 2);
+            list.Print();
+            Console.WriteLine(list.CountOccurences(2) == 2);
+            list.Clear();
+            Console.WriteLine(list.CountOccurences(2) == 0);
+
+            Console.ReadKey();
+        }
+    }
+
+    interface SimpleArrayList
+    {
+        void Add(int n); // toevoegen aan het einde van de lijst, mits de lijst niet vol is
+        int Get(int index); // haal de waarde op van een bepaalde index
+        void Set(int index, int n); // wijzig een item op een bepaalde index
+        void Print(); // print de inhoud van de list
+        void Clear(); // maak de list leeg
+        int CountOccurences(int n); // tel hoe vaak het gegeven getal voorkomt
+    }
+
+    class ArrayList : SimpleArrayList
+    {
+        int[] _array;
+        int _nextIndex;
+
+        public ArrayList(int capacity)
+        {
+            _array = new int[capacity];
+            _nextIndex = 0;
+        }
+
+        public void Add(int n)
+        {
+            if (_nextIndex != _array.Length)
+            {
+                _array[_nextIndex] = n;
+                _nextIndex++;
+            }
+            else
+            {
+                throw new Exception("The array has reached its limit!");
+            }
+        }
+
+        public void Clear()
+        {
+            _array = new int[_array.Length];
+            _nextIndex = 0;
+        }
+
+        public int CountOccurences(int n)
+        {
+            var occurences = 0;
+            for(var i= 0; i < _array.Length; i++)
+            {
+                if (_array[i] == n)
+                {
+                    occurences++;
+                }
+            }
+            return occurences;
+        }
+
+        public int Get(int index)
+        {
+            if (index < 0 || index > _array.Length) throw new Exception("Invalid index!");
+            return _array[index];
+        }
+
+        public void Print()
+        {
+            Console.Write("Itemcount: " + _nextIndex + ", Items: ");
+            for (var i = 0; i < _nextIndex; i++)
+            {
+                Console.Write(_array[i] + ", ");
+            }
+            Console.Write(Environment.NewLine);
+        }
+
+        public void Set(int index, int n)
+        {
+            if (index < 0 || index > _array.Length) throw new Exception("Invalid index!");
+            _array[index] = n;
+        }
+    }
+}
