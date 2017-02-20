64215aa7657884a6e6ec6b88c0bd778b9b55a875
Thom Brink <thombrink@hotmail.com>
2017-02-20 19:26:26 +0100

    implemented binary and recursive patterns

---
+++ Binary/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine(Convert.ToString(2147483647, 2));
+
+            for(var i = 0; i <= 18; i++)
+            {
+                Console.WriteLine(i + " = " + Convert.ToString(i, 2) + " | " + Convert.ToString(1 << i) + " = " + Convert.ToString(1 << i, 2));
+            }*/
+
+            var integer = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("het quadraat van " + integer + " is " + (integer << 3));
+            Console.WriteLine("de wortel van " + integer + " is " + (integer >> 3));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Recursive/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int>(new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+            PrintForward1(list, 3);
+            Console.Write(Environment.NewLine);
+            PrintBackward1(list, 3);
+
+            Console.Write(Environment.NewLine);
+            Console.Write(Environment.NewLine);
+
+            list = new List<int>(new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+            PrintForward2(list, 3);
+            Console.Write(Environment.NewLine);
+            list = new List<int>(new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+            PrintBackward2(list, 3);
+
+            Console.ReadKey();
+        }
+
+        static void PrintForward1(List<int> theList, int i)
+        {
+            if (i < theList.Count)
+            {
+                Console.Write(theList[i] + ", ");
+                PrintForward1(theList, i + 1);
+            }
+        }
+
+        static void PrintForward2(List<int> theList, int i)
+        {
+            if (i != theList.Count)
+            {
+                Console.Write(theList[i] + ", ");
+                theList.RemoveAt(i);
+                PrintForward2(theList, i);
+            }
+        }
+
+        static void PrintBackward1(List<int> theList, int i)
+        {
+            if (i < theList.Count)
+            {
+                PrintBackward1(theList, i + 1);
+                Console.Write(theList[i] + ", ");
+            }
+        }
+
+        static void PrintBackward2(List<int> theList, int i)
+        {
+            if (i != theList.Count)
+            {
+                Console.Write(theList[theList.Count - 1] + ", ");
+                theList.RemoveAt(theList.Count - 1);
+                PrintBackward1(theList, i);
+            }
+        }
+    }
+}
