2830c0193af5053833f6525f9b2a5dd22dea36a5
Thom Brink <thombrink@hotmail.com>
2017-03-21 22:43:16 +0100

    finished the practice test

--- PracticeTest/Program.cs
+++ PracticeTest/Program.cs
@@ -61,7 +61,7 @@ namespace PracticeTest {
             tree.AddNode(6);
             tree.AddNode(2);
             tree.AddNode(8);
-            //tree.AddNode(1);
+            tree.AddNode(1);
             tree.AddNode(4);
             tree.AddNode(3);
 
@@ -70,6 +70,7 @@ namespace PracticeTest {
             var almostSmallest = tree.GeefEenNaKleinsteElement();
 
             //Console.WriteLine(tree.ToString());
+            Console.SetCursorPosition(0, 30);
             #endregion
             #region Opdracht 4
             var heap = new Heap();
@@ -77,8 +78,57 @@ namespace PracticeTest {
             heap.Add(4);
             heap.Add(22);
             heap.Add(2);
+            heap.Add(2);
+            heap.Add(2);
+            heap.Add(2);
+            heap.Add(2);
+            heap.Add(2);
 
             Console.WriteLine(heap.ToString());
+
+            Console.WriteLine("Is complete: " + heap.IsComplete());
+
+            heap.BreakHeap();
+
+            Console.WriteLine("Is complete: " + heap.IsComplete());
+
+            Console.WriteLine("Is max heap: " + heap.IsMaxHeap());
+
+            var myBeautifulTreee = heap.ConverteerNaarBoom();
+
+            #endregion
+            #region Opdracht 5
+
+            var graph = new Graph();
+            graph.AddEdge("A", "B");
+            graph.AddEdge("A", "G");
+
+            graph.AddEdge("C", "B");
+
+            graph.AddEdge("D", "F");
+            graph.AddEdge("D", "G");
+
+            graph.AddEdge("E", "C");
+
+            graph.AddEdge("F", "F");
+
+            graph.AddEdge("G", "D");
+            graph.AddEdge("G", "F");
+            graph.AddEdge("G", "H");
+
+            graph.AddEdge("H", "E");
+            graph.AddEdge("H", "K");
+
+            graph.AddEdge("K", "G");
+
+            //graph.Calculate("G");
+
+            Console.WriteLine("E has cycle: " + graph.HasCycle("E"));
+            Console.WriteLine("K has cycle: " + graph.HasCycle("K"));
+
+            graph.ToonCycle();
+
+            graph.ToonAfstand("G");
 
             #endregion
 
@@ -104,7 +154,7 @@ namespace PracticeTest {
                 Console.Write("A");
             }
 
-            PrintLetters(p -1, q - 1);
+            PrintLetters(p - 1, q - 1);
 
             if (q > 0) {
                 Console.Write("Z");
@@ -319,13 +369,13 @@ namespace PracticeTest {
             Console.SetCursorPosition(x, y);
             Console.Write($"[{currentNode.Data}]");
 
-            if(currentNode.LeftNode != null) {
+            if (currentNode.LeftNode != null) {
                 Console.SetCursorPosition(x - 3, y + 2);
                 Console.Write("/");
                 PrintTree(currentNode.LeftNode, x - 6, y + 4);
-                
+
             }
-            if(currentNode.RightNode != null) {
+            if (currentNode.RightNode != null) {
                 Console.SetCursorPosition(x + 5, y + 2);
                 Console.Write(@"\");
                 PrintTree(currentNode.RightNode, x + 6, y + 4);
@@ -340,7 +390,7 @@ namespace PracticeTest {
             TreeNode currentNode = RootNode;
 
             while (currentNode != null) {
-                if(currentNode.Data < almostSmallest) {
+                if (currentNode.Data < almostSmallest) {
                     if (currentNode.Data < smallest) {
                         almostSmallest = smallest;
                         smallest = currentNode.Data;
@@ -369,16 +419,12 @@ namespace PracticeTest {
 
     class Heap {
         int[] _array = new int[10];
-        int _length = 0;
-
-        public Heap() {
-
-        }
+        int _length = 1;
 
         public void Add(int value) {
             var i = _length;
             var pos = i;
-            for (; _array[i / 2] > value && i > 0; i /= 2) {
+            for (; _array[i / 2] > value && i > 1; i /= 2) {
                 pos = i / 2;
                 _array[i] = _array[pos];
             }
@@ -386,10 +432,51 @@ namespace PracticeTest {
             _length++;
         }
 
+        public void BreakHeap() {
+            _array[2] = 0;
+        }
+
+        public bool IsComplete() {
+            // Geeft true indien de BT compleet is en anders false
+            var hasPassedEnd = false;
+            for (var i = 1; i < _length; i++) {
+                if (hasPassedEnd && _array[i] > 0) return false;
+                if (_array[i] == 0) { hasPassedEnd = true; }
+            }
+            return true;
+        }
+
+        public bool IsMaxHeap() {
+            //Geeft true indien de BT een maxheap is en anders false
+            for (var i = 1; i * 2 + 1 < _array.Length; i++) {
+                var parent = _array[i];
+                var left = _array[i * 2];
+                var right = _array[i * 2 + 1];
+
+                if (left > parent || right > parent) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public BinaryTree ConverteerNaarBoom() {
+            var tree = new BinaryTree();
+            for (var i = 1; i * 2 + 1 < _array.Length; i++) {
+                tree.AddNode(_array[i]);
+                tree.AddNode(_array[i * 2]);
+                tree.AddNode(_array[i * 2 + 1]);
+            }
+            return tree;
+        }
+
         public override string ToString() {
             var sb = new StringBuilder();
 
-            for(var i = 0; i < _length; i++) {
+            sb.Append("#, ");
+
+            for (var i = 1; i < _length; i++) {
                 sb.Append(_array[i] + ", ");
             }
 
@@ -398,6 +485,175 @@ namespace PracticeTest {
             }
 
             return sb.ToString();
+        }
+    }
+
+    public class Graph {
+        private Dictionary<string, Node> nodeMap = new Dictionary<string, Node>();
+
+        public void AddEdge(string sourceName, string destinationName, int cost = 1) {
+            Node source = GetNode(sourceName);
+            Node destination = GetNode(destinationName);
+            source.Edges.Add(new Edge(destination, cost));
+        }
+
+        public void Calculate(string startNodeName) {
+            Node startNode = GetNode(startNodeName);
+
+            var nodeQueue = new Queue<Node>();
+            nodeQueue.Enqueue(startNode);
+
+            foreach (var node in nodeMap.Values) {
+                node.Processed = false;
+                node.Distance = int.MaxValue;
+                node.Previous = null;
+            }
+
+            startNode.Distance = 0;
+
+            while (nodeQueue.Any()) {
+                var currNode = nodeQueue.Dequeue();
+
+                if (currNode.Processed) continue;
+                currNode.Processed = true;
+
+                foreach (var edge in currNode.Edges) {
+                    var destination = edge.Destination;
+                    var newDistance = currNode.Distance + edge.Cost;
+
+                    //if (destination.Distance == int.MaxValue) {
+                    if (newDistance < destination.Distance) {
+                        destination.Distance = newDistance;
+                        destination.Previous = currNode;
+
+                        nodeQueue.Enqueue(destination);
+                    }
+                }
+            }
+        }
+
+        public bool HasCycle(string startNodeName) {
+            Node startNode = GetNode(startNodeName);
+
+            var nodeQueue = new Queue<Node>();
+            nodeQueue.Enqueue(startNode);
+
+            foreach (var node in nodeMap.Values) {
+                node.Processed = false;
+                node.Distance = int.MaxValue;
+                node.Previous = null;
+            }
+
+            startNode.Distance = 0;
+
+            while (nodeQueue.Any()) {
+                var currNode = nodeQueue.Dequeue();
+
+                if (currNode.Processed) continue;
+                currNode.Processed = true;
+
+                foreach (var edge in currNode.Edges) {
+                    if (startNode.Name == edge.Destination.Name) return true;
+
+                    nodeQueue.Enqueue(edge.Destination);
+                }
+            }
+
+            return false;
+        }
+
+        public bool IsConnected() {
+            Node startNode = nodeMap.First().Value;
+
+            var nodeQueue = new Queue<Node>();
+            nodeQueue.Enqueue(startNode);
+
+            foreach (var node in nodeMap.Values) {
+                node.Processed = false;
+                node.Distance = int.MaxValue;
+                node.Previous = null;
+            }
+
+            while (nodeQueue.Any()) {
+                var currNode = nodeQueue.Dequeue();
+
+                if (currNode.Processed) continue;
+                currNode.Processed = true;
+
+                foreach (var edge in currNode.Edges) {
+                    var destination = edge.Destination;
+
+                    if (destination.Distance == int.MaxValue) {
+                        nodeQueue.Enqueue(destination);
+                    }
+                }
+            }
+
+            return nodeMap.All(x => x.Value.Processed);
+        }
+
+        private Node GetNode(string name) {
+            Node result;
+            if (nodeMap.ContainsKey(name)) {
+                result = nodeMap[name];
+            }
+            else {
+                result = new Node(name);
+                nodeMap.Add(name, result);
+            }
+            return result;
+        }
+
+        public void Print() {
+            var sb = new StringBuilder();
+            foreach (var node in nodeMap) {
+                sb.AppendLine(node.Key);
+
+                foreach (var edge in node.Value.Edges) {
+                    sb.AppendLine("- " + edge.Destination.Name);
+                }
+
+                sb.AppendLine();
+            }
+            Console.Write(sb.ToString());
+        }
+
+        public void ToonCycle() {
+            foreach (var node in nodeMap) {
+                Console.WriteLine("Node: " + node.Key + ", Has cycle: " + HasCycle(node.Key));
+            }
+        }
+
+        public void ToonAfstand(string startNodeName) {
+            Calculate(startNodeName);
+
+            foreach (var node in nodeMap) {
+                if (node.Key == startNodeName) continue;
+
+                Console.WriteLine("Node: " + node.Key + ", Distance: " + (node.Value.Distance == int.MaxValue ? -1 : node.Value.Distance));
+            }
+        }
+
+        public class Node {
+            public string Name { get; set; }
+            public List<Edge> Edges { get; set; } = new List<Edge>();
+            public int Distance { get; set; }
+            public Node Previous { get; set; }
+            public bool Processed { get; set; }
+
+            public Node(string name) {
+                Name = name;
+            }
+        }
+
+        public class Edge {
+            public Node Destination { get; set; }
+            public int Cost { get; set; }
+
+            public Edge(Node destination, int cost) {
+                Destination = destination;
+                Cost = cost;
+            }
         }
     }
 }
